23d904289056e2f3056ea98fe42ecdf7dbfb3cb7
Yuta Tokitake <toki211@gmail.com>
2014-09-27 18:00:28 +0900

    Initial Commit

---
+++ Meshitero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meshitero
+//
+//  Created by 時武佑太 on 2014/09/27.
+//  Copyright (c) 2014年 Yuta Tokitake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
